f3168e44e94267a873b6f7a1ae6731c525492bd5
SultanIndian007 <amantiwari201608@gmail.com>
2022-10-02 21:12:17 +0530

    first commit

---
+++ src/utils/fetchFromAPI.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+
+export const BASE_URL = 'https://youtube-v31.p.rapidapi.com';
+
+const options = {
+  params: {
+    maxResults: '50',
+  },
+  headers: {
+    'X-RapidAPI-Key': '0429f03a17msh897cb9577d9568ep1fa7f7jsn45ad5a66ed49',
+    'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com',
+  },
+};
+
+export const fetchFromAPI = async (url) => {
+  const { data } = await axios.get(`${BASE_URL}/${url}`, options);
+
+  return data;
+};
